f7222bbf9a659ea1336b5efc5f5e2793ee125dfb
ChrisT-CC <cristaecc@gmail.com>
2022-01-14 18:40:17 +0000

    Update __init__.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,8 +1,9 @@
+""" initialization file """
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# In order to actually use any of our hidden environment variables, we need to
+# in order to actually use any of our hidden environment variables, we need to
 # import the 'env' package. However, since we are not pushing the env.py file
 # to GitHub, this file will not be visible once deployed to Heroku, and will
 # throw an error. This is why we need to only import 'env' if the OS can find
@@ -10,10 +11,15 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
 
-# Create an instance of Flask class
+# create an instance of Flask class
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+# add a conditional check for Heroku's Postgres database
+if os.environ.get("DEVELOPMENT") == True:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 # Create an instance of the imported SQLAlchemy() class
 db = SQLAlchemy(app)
